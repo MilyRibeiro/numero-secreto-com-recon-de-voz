adfc17a2ad8613e1f6ae6009a0061c2ac149b802
Mylena Marques Ribeiro <ribeiro.mylena1@gmail.com>
2024-11-24 13:33:18 -0300

    Sorteio do número aleatório

--- app js/sortear-numero.js
+++ app js/sortear-numero.js
@@ -1,9 +1,16 @@
-const numeroSecreto = gerarNumeroAleatorio();
-const menorValor = 1;
-const maiorValor = 10;
+const menorValor = 1
+const maiorValor = 1000
+const numeroSecreto = gerarNumeroAleatorio()
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() *100);
+    return parseInt(Math.random() * maiorValor + 1)
 }
 
-console.log('O número secreto é: ', numeroSecreto);
+console.log('Número Secreto:', numeroSecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
+
